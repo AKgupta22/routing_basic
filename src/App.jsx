8ef8b2d884df003e75a45e256e4eb615ab0145c3
arunkumar2011 <akumar6@rh.com>
2022-11-29 18:18:51 +0530

    version 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Component/Home";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
+import Message from "./Component/Message";
+import SingleMessage from "./Component/SingleMessage";
+import Navbar from "./Component/Navbar";
 function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/message" element={<Message />} />
+        <Route path="/message/:q" element={<SingleMessage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
